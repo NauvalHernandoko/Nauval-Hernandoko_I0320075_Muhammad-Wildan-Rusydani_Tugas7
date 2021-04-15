a14f256e4155de644f5da62fa955847e0347f23b
Nauval Hernandoko <nauvalhernadoko@gmail.com>
2021-04-15 22:10:20 +0700

    Exercise

---
+++ I0320075_Exercise7.1.py
@@ -0,0 +1,12 @@
+#Exercise7.1
+nama = "Posi"
+tahun = "2018"
+
+def help():
+    nama = "lab posi"
+    tahun = "18"
+    print("nama= %s" % nama)
+    print("tahun= %s" % tahun)
+print("nama= %s" % nama)
+print("tahun= %s" % tahun)
+help()
---
+++ I0320075_Exercise7.13.py
@@ -0,0 +1,4 @@
+str = "Hello World"
+print("str: " + str)
+print("str replace o: ", str.replace('o'))
+print("str replace l: ", str.replace('l'))
